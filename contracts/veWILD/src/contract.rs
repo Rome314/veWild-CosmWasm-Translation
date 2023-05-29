abe785e2df46c8fe6b8a276eadf49d0015a0d9e6
Rome314 <35641068+Rome314@users.noreply.github.com>
2023-05-30 01:29:21 +0600

    fix Cargo

--- contracts/veWILD/src/contract.rs
+++ contracts/veWILD/src/contract.rs
@@ -3,12 +3,24 @@ use crate::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
 use crate::state::*;
 use cosmwasm_std::{to_binary, Binary, Deps, DepsMut, Env, MessageInfo, Response, StdResult};
 
+#[cfg_attr(not(feature = "library"), entry_point)]
 pub fn instantiate(
     deps: DepsMut,
-    _env: Env,
-    _info: MessageInfo,
+    env: Env,
+    info: MessageInfo,
     msg: InstantiateMsg,
 ) -> StdResult<Response> {
+
+    
+
+    Ok(Response::default())
+}
+
+
+mod utils {
+    use super::*;
+
+    pub fn 
 }
 
 mod query {
@@ -17,6 +29,7 @@ mod query {
     use super::*;
 }
 
+#[cfg_attr(not(feature = "library"), entry_point)]
 pub fn execute(
     deps: DepsMut,
     _env: Env,
@@ -33,6 +46,7 @@ mod exec {
     use super::*;
 }
 
+#[cfg_attr(not(feature = "library"), entry_point)]
 pub fn query(deps: Deps, _env: Env, msg: QueryMsg) -> StdResult<Binary> {
     use QueryMsg::*;
 

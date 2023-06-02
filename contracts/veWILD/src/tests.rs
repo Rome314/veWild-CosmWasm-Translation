dcccf3a81385a3eccb6620ffedd573c632157775
Rome314 <35641068+Rome314@users.noreply.github.com>
2023-06-03 03:45:17 +0600

    first steps to testing

--- contracts/veWILD/src/tests.rs
+++ contracts/veWILD/src/tests.rs
@@ -4,14 +4,14 @@ mod tests {
     use crate::msg::*;
     use cosmwasm_std::{ Addr, Uint64 };
     use cw_multi_test::{ App, ContractWrapper, Executor };
-    use cosmwasm_std::testing::{ mock_dependencies, mock_env, mock_info, MOCK_CONTRACT_ADDR };
+    use cosmwasm_std::testing::{ mock_dependencies, mock_env, mock_info };
 
     #[test]
     fn proper_instantiation() {
         let mut app = App::default();
 
         let code = ContractWrapper::new(execute, instantiate, query);
-        let code_id = app.store_code(Box::new(code));
+        let _code_id = app.store_code(Box::new(code));
 
         let resp = instantiate(
             mock_dependencies().as_mut(),
@@ -28,16 +28,7 @@ mod tests {
         println!("{:?}", resp.attributes);
         assert_eq!(0, resp.messages.len());
 
-        // let init_resp = app.instantiate_contract(
-        //     code_id,
-        //     Addr::unchecked("owner"),
-        //     &(InstantiateMsg {
-        //         locked_token: Addr::unchecked("cw20"),
-        //         distribution_period: Uint64::from(1000 as u16),
-        //     }),
-        //     &[],
-        //     "Contract",
-        //     None
-        // );
+        //TODO: check events
+        //TODO: check state
     }
 }
